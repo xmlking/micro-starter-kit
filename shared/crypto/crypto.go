549f276adf27adece9850b2141447d564665a362
Sumanth Chinthagunta <xmlking@gmail.com>
2019-06-30 22:05:10 -0700

    Create crypto.go

---
+++ shared/crypto/crypto.go
@@ -0,0 +1,80 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+
+  "github.com/xmlking/micro-starter-kit/shared/util"
+)
+
+// AesEncrypt takes in a string, a key and returns encrypted string
+func AesEncrypt(orig string, key string) (string, error) {
+	// generate a new aes cipher using our 32 byte long key
+	c, err := aes.NewCipher([]byte(key))
+	// if there are any errors, handle them
+	if err != nil {
+		return "", err
+	}
+
+	// gcm or Galois/Counter Mode, is a mode of operation
+	// for symmetric key cryptographic block ciphers
+	// - https://en.wikipedia.org/wiki/Galois/Counter_Mode
+	gcm, err := cipher.NewGCM(c)
+	// if any error generating new GCM
+	// handle them
+	if err != nil {
+		return "", err
+	}
+
+	// creates a new byte array the size of the nonce
+	// which must be passed to Seal
+	nonce := make([]byte, gcm.NonceSize())
+	// populates our nonce with a cryptographically secure
+	// random sequence
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	// here we encrypt our text using the Seal function
+	// Seal encrypts and authenticates plaintext, authenticates the
+	// additional data and appends the result to dst, returning the updated
+	// slice. The nonce must be NonceSize() bytes long and unique for all
+	// time, for a given key.
+	// signatured := commonutils.HexEncodeToString()
+	return util.Base64Encode(gcm.Seal(nonce, nonce, []byte(orig), nil)), nil
+
+}
+
+// AesDecrypt takes in a encripted string, a key and returns decrypted string
+func AesDecrypt(cryted string, key string) (string, error) {
+	ciphertext, err := util.Base64Decode(cryted)
+	// if our program was unable to read the file
+	// print out the reason why it can't
+	if err != nil {
+		return "", err
+	}
+
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return "", err
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
