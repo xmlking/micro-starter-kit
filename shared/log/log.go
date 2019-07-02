4ed272ff2dba9acc23528e215a53dfb97671d9c6
Sumanth Chinthagunta <xmlking@gmail.com>
2019-07-01 18:23:09 -0700

    switching to logrus as default logger

--- shared/log/log.go
+++ shared/log/log.go
@@ -1,88 +1,26 @@
 package log
 
-// TODO https://github.com/uknth/go-base/blob/master/base/log/log.go
-// TODO https://github.com/pmker/vash/blob/master/common/log/log.go
-
 import (
-	"runtime"
-	"strings"
-
 	"github.com/micro/go-micro/config"
-	"github.com/micro/go-micro/util/log"
+	microlog "github.com/micro/go-micro/util/log"
 	"github.com/sirupsen/logrus"
+	micrologrus "github.com/tudurom/micro-logrus"
 )
 
-// Log is shared
-var Log Logger
-
 func init() {
-	Log = NewLogger()
-	log.SetLogger(Log)
+	logrusLogger := newLogger()
+	// also set same Formatter and Level for logrus's global logger
+	logrus.SetFormatter(logrusLogger.Formatter)
+	logrus.SetLevel(logrusLogger.Level)
+
+	microLogger := micrologrus.NewMicroLogrus(logrusLogger)
+	microlog.SetLogger(microLogger)
 }
 
-func parentCaller() string {
-	pc, _, _, ok := runtime.Caller(4)
-	fn := runtime.FuncForPC(pc)
-	if ok && fn != nil {
-		return fn.Name()
-	}
-	return ""
-}
-
-// Logger interface for micro
-type Logger interface {
-	Log(v ...interface{})
-	Logf(format string, v ...interface{})
-
-	WithField(key string, value interface{}) *logrus.Entry
-	WithFields(fields logrus.Fields) *logrus.Entry
-	WithError(err error) *logrus.Entry
-
-	Debugf(format string, args ...interface{})
-	Infof(format string, args ...interface{})
-	Printf(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
-	Warningf(format string, args ...interface{})
-	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})
-	Panicf(format string, args ...interface{})
-
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Print(args ...interface{})
-	Warn(args ...interface{})
-	Warning(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
-	Panic(args ...interface{})
-}
-
-type logger struct {
-	logrus.Logger
-}
-
-func (l *logger) Log(v ...interface{}) {
-	pc := parentCaller()
-	if strings.HasSuffix(pc, "Fatal") {
-		l.Fatal(v...)
-	} else {
-		l.Info(v...)
-	}
-}
-
-func (l *logger) Logf(format string, v ...interface{}) {
-	pc := parentCaller()
-	if strings.HasSuffix(pc, "Fatalf") {
-		l.Fatalf(format, v...)
-	} else {
-		l.Infof(format, v...)
-	}
-}
-
-// NewLogger create new logrus logger from config
+// newLogger create new logrus logger from config
 // log level: panic, fatal, error, warn, info, debug, trace
 // log format: json, text
-func NewLogger() Logger {
+func newLogger() *logrus.Logger {
 	level := config.Get("log", "level").String("info")
 	format := config.Get("log", "format").String("text")
 	lslog := logrus.New()
@@ -103,9 +41,17 @@ func NewLogger() Logger {
 	}
 
 	lslog.WithFields(logrus.Fields{
-		"level":  logLevel,
-		"format": format,
+		"logLevel": logLevel,
+		"format":   format,
 	}).Debug("Logger set to Logrus with:")
 
-	return &logger{*lslog}
+	return lslog
 }
+
+// NewLogger create new logrus logger from config and return FieldLogger interface
+// log level: panic, fatal, error, warn, info, debug, trace
+// log format: json, text
+func NewLogger() logrus.FieldLogger {
+	return newLogger()
+}
+
