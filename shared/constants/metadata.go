4a8b78ee5417c7f563a0f0dd1d8173335e37c573
Sumanth Chinthagunta <xmlking@gmail.com>
2020-02-17 13:36:35 -0800

    refactor(wrapper): using all CAPITAL for metadata keys i.e., TransID = "TRANS-ID"

--- shared/constants/metadata.go
+++ shared/constants/metadata.go
@@ -4,6 +4,6 @@ package constants
 
 const (
 	// There are certain requirements for metadata to be passed in the http header:
-	// Just use all small letters for God's sake
-	TransID = "trans-id"
+	// Just use all capital letters for God's sake
+	TransID = "TRANS-ID"
 )
